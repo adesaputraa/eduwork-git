ac7ae6b85d9259352d268c8542405dbe05435a80
adesaputraa <adesaputra0928@gmail.com>
2022-08-25 14:28:09 +0700

    upload folder dan tugas

---
+++ QA - JS Variable/Tugas.js
@@ -0,0 +1,15 @@
+
+let x = "10" + 10 + 10;
+let y = 10 + 10 + "10";
+console.log(x + y);
+
+// hasilnya 1010102010 karna menggunakan kutip 2 ("")
+
+/* 
+let x = 10 + 10 + 10;
+let y = 10 + 10 + 10;
+let z = x + y;
+console.log(z); 
+hasilnya 60 karna tidak ada type data string atau kutip 2("")
+
+  */
---
+++ QA - JS Variable/variable.js
@@ -0,0 +1,22 @@
+/* const name = "ade";
+name = "Eduwork";
+console.log(name); */
+
+/* variable const (tetap)
+sebuah value tidak akan dapat berubah kecuali menggunkan tipe data array dan objeck
+atau disebut dengan mutable */
+
+//  Mutable with object
+/* const student = {
+    name: "ade",
+    class: 2
+}
+
+student.faculty = "sistem informasi";
+console.log(student); */
+
+
+// Mutable with array 
+const students = ['ade', 'dian', 'agung', 'agus'];
+students.push('div');
+console.log(students);
