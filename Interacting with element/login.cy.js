40204254d208a533984eaafea10de91151d1b700
adesaputraa <adesaputra0928@gmail.com>
2022-09-04 17:01:10 +0700

    tugas interacting

---
+++ Interacting with element/login.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+
+    it('Visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+    });
+
+    it('Should fill username', () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')
+    });
+
+    it('Should fill password', () => {
+        cy.get('input[name=user_password]').clear()
+        cy.get('input[name=user_password]').type('password')
+    });
+
+
+    // INI TUGAS mengaktifkan fitur checkbox keep me signed in
+    it('Should fill Checkbox', () => {
+        cy.get('input[name="user_remember_me"]').click();
+        cy.get('[type="checkbox"]').type('checkbox')
+    });
+});
