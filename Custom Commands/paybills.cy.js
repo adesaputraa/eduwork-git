ba4cde2426b6e05c2d371e6ee6460391ddf3c0cc
adesaputraa <adesaputra0928@gmail.com>
2022-09-11 21:38:28 +0700

    tugas

---
+++ Custom Commands/paybills.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe('Working with inputs', () => {
+    it('visit the website', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.url().should('include', 'login.html')
+    });
+
+    it('Should fill username', () => {
+        cy.get('#user_login').clear()
+        cy.get('#user_login').type('username')
+    });
+
+    it('should fill password', () => {
+        cy.get('input[name="user_password"]').clear()
+        cy.get('input[name="user_password"]').type('password')
+    });
+
+
+    it('click sign in and navigate to Pay Bills', () => {
+
+        cy.get('input[type="submit"]').click()
+
+        cy.get('a[href="/bank/redirect.html?url=pay-bills.html"]').click()
+    })
+
+    it('input data for payment', () => {
+        cy.paybills()
+    })
+
+});
